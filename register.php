4989ea09485b09a22364825ba2f7d50f27d57bee
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-25 20:03:13 +0100

    register form created

---
+++ register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account in PHP</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<div class="form-container">
+        <div class="overlay">
+
+        </div>
+        <div class="titleDev">
+            <h1 class="title">Register</h1>
+            <span class="subTitle">Thanks for choosing us!</span>   
+        </div>
+        <form action="" method="post">
+            <div class="rows grid">
+                <div class="row">
+                    <label for="username">Username: </label>
+                    <input type="text" id="username" name="username" placeholder="Enter username" required>
+                </div>
+                <div class="row">
+                    <label for="email">Email: </label>
+                    <input type="email" id="email" name="email" placeholder="Enter email" required>
+                </div>
+                <div class="row">
+                    <label for="phone">Phone Number: </label>
+                    <input type="tel" id="phone" name="phone" placeholder="Enter phone number" required>
+                </div>
+                <div class="row">
+                    <label for="password">Password: </label>
+                    <input type="password" id="password" name="password" placeholder="Enter password" required>
+                </div>
+                <div class="row">
+                    <input type="submit" id="submitBtn" name="submit" value="Register">
+                    <span class="registerLink">Don't have an account? <a href="register.php">Register</a> </span>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
