cf9531e21b1f4f9020d6012584c3adfa399800df
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-25 18:07:47 +0100

    form setup

--- index.php
+++ index.php
@@ -7,6 +7,22 @@
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    
+    <div class="form-container">
+        <div class="overlay">
+
+        </div>
+        <div class="titleDev">
+            <h1 class="title">Login</h1>
+            <span class="subTitle">Welcome back!</span>   
+        </div>
+        <form action="" method="post">
+            <div class="row grid">
+                <div class="row">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username">
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
